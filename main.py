d1f042614df419663a88b260118275a45b543489
Uzair <uzairullazi@gmail.com>
2024-08-04 16:19:04 +0800

    Update main.py
    Revise main.py to import content from each section base on each section file

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ with st.sidebar:
     selected = option_menu(
         menu_title= None,   # To ignore menu title    
         # menu_title= "Main Menu", # To give menu title
-        options= ["Home","Projects","Activities","Contact"],
-        icons= ["house-door-fill","rocket-takeoff","clipboard2-check","whatsapp"]
+        options= ["Home","Projects","Activities","Certificates","Contact"],
+        icons= ["house-door-fill","rocket-takeoff","clipboard2-check","award","whatsapp"]
     )
 
+#profile image
+profile = "profile.png"
+
+#Home section
 if selected == "Home":
     col1, col2 = st.columns(2)
     with col1:
@@ -16,11 +20,30 @@ if selected == "Home":
         st.header ("|:blue[Project Manufacturer] |:blue[IATF Project]")
         st.header ("|:blue[Data Analysis]")
         st.header ("from :blue[Johor Bahru], :blue[Malaysia]")
+        st.write(":white[Nearly a decade of experience in product/process manufacturing, especially rubber. Led efforts to meet automotive/IATF needs. Coordinated manufacturing systems & data analysis (Excel, Power Query). Participated in ISO 9001 & IATF 16949 internal audits to ensure compliance.]")
     with col2:
-        st.image('profile.png', caption='Nice to meet you.')
+        st.image(profile, caption='Nice to meet you.')
+
+#Projects section
 if selected == "Projects":
     st.title(f"{selected}")
+    from projects import projects_section #import project_section from projects.py
+    projects_section()
+
+#Activities section
 if selected == "Activities":
     st.title(f"{selected}")
+    from activities import activities_section #import activities_section from activities.py
+    activities_section() 
+
+#Certificates section
+if selected == "Certificates":
+    st.title(f"{selected}")
+    from certificates import certificates_section #import certificates_section from certificates.py
+    certificates_section()
+
+#contact section
 if selected == "Contact":
     st.title(f"{selected}")
+    from contact import contact_section #import contact_section from contact.py
+    contact_section()
